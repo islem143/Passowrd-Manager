d16f6a0d3742fcc255a0599ed509af5fbfcb5b2b
islem <djennaneislempro@gmail.com>
2021-09-06 19:20:55 +0100

    remove unneccesary files

--- test.py
+++
@@ -1,9 +0,0 @@
-import bcrypt
-
-password=b'islem'     
-salt=bcrypt.gensalt()
-hashed=bcrypt.hashpw(password,b'$2b$12$SLiGxl.1RlLzQJfX6qZmjO')
-
-print(b'$2b$12$SLiGxl.1RlLzQJfX6qZmjOgFItk73gSY8TeN0as2iakJHJXqKmGkm'==b'$2b$12$SLiGxl.1RlLzQJfX6qZmjOgFItk73gSY8TeN0as2iakJHJXqKmGkm')
-
-#print(bcrypt.checkpw(password,hashed))
