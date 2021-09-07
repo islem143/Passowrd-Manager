ec74710d155abd2020eee8b47e040a95a5c47447
islem <djennaneislempro@gmail.com>
2021-09-07 18:18:55 +0100

    remove test file

--- test.py
+++
@@ -1,9 +0,0 @@
-import bcrypt
-
-password=b'islem'     
-salt=bcrypt.gensalt()
-print(salt)
-hashed=bcrypt.hashpw(password,salt)
-# password2=b"islem"
-# hashed2=bcrypt.hashpw(password2,salt)
-print(bcrypt.checkpw(b"islem",hashed))
